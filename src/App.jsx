da9942c5ff30bac95dc0c50ef1fa0ebf91ccc0d6
AMPHIS-POE <heinejjw@naver.com>
2025-09-14 19:59:55 +0900

    planner test

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Poe1Landing from './pages/POE1/Poe1Landing';
 import Poe2Landing from './pages/POE2/Poe2Landing';
 import ContentGuidePage from './components/GuidePage/ContentGuidePage';
 import BackToTopButton from './components/BackToTopButton';
+import BaseItemTester from './pages/POE1/Feature/Build/BaseItemTester.jsx';
 import './App.css';
 
 const LoadingSpinner = () => {
@@ -31,9 +32,10 @@ function AppContent() {
             <Route path="/" element={<MainLanding />} />
             <Route path="/poe1/*" element={<Poe1Landing lang={lang} />} />
             <Route path="/poe2/*" element={<Poe2Landing lang={lang} />} />
-
             <Route path="/poe1/guides/:slug" element={<ContentGuidePage lang={lang} />} />
             <Route path="/poe2/guides/:slug" element={<ContentGuidePage lang={lang} />} />
+            <Route path="/poe1/build" element={<BaseItemTester />} />
+
           </Routes>
         </main>
         <Footer lang={lang} />
